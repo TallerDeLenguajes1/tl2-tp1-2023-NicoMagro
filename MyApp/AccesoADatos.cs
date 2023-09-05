9be834ec9e64a79aedfb231dfb8fdae34ee01694
NicoMagro <anicomagro@gmail.com>
2023-09-05 15:51:26 -0300

    Mejorando clase datos y haciendo la interfaz de usuario

--- MyApp/AccesoADatos.cs
+++ MyApp/AccesoADatos.cs
@@ -11,25 +11,27 @@ public class AccesoADatos
     {
     }
 
-    public virtual void leerCadete()
+    public virtual List<Cadete> leerCadete()
     {
         System.Console.WriteLine("Metodo por defecto de lectura.");
+        return new List<Cadete>();
     }
 
-    public virtual void leerCadeteria()
+    public virtual List<Cadeteria> leerCadeteria()
     {
         System.Console.WriteLine("Metodo por defecto de lectura.");
+        return new List<Cadeteria>();
     }
 }
 
 public class AccesoCSV : AccesoADatos
 {
-    public AccesoCSV(string rutaArchivo) : base()
+    public AccesoCSV() : base()
     {
 
     }
 
-    public override void leerCadeteria()
+    public override List<Cadeteria> leerCadeteria()
     {
         try
         {
@@ -43,11 +45,11 @@ public class AccesoCSV : AccesoADatos
 
                 if (encabezados != null)
                 {
+                    var listaCadeteria = new List<Cadeteria>();
                     while (!reader.EndOfStream)
                     {
                         var linea = reader.ReadLine();
                         var valores = linea.Split(',');
-                        var listaCadeteria = new List<Cadeteria>();
                         if (valores.Length == encabezados.Length)
                         {
                             var cadeteria = new Cadeteria(valores[0], valores[1]);
@@ -61,18 +63,26 @@ public class AccesoCSV : AccesoADatos
                             Console.WriteLine("La línea no tiene el formato correcto.");
                         }
                     }
+                    System.Console.WriteLine("Cadeteria cargada correctamente.");
+                    return listaCadeteria;
+                }
+                else
+                {
+                    System.Console.WriteLine("La cadeteria no se cargo correctamente.");
+                    return new List<Cadeteria>();
                 }
             }
         }
         catch (FileNotFoundException)
         {
             Console.WriteLine($"El archivo CSV en la ruta '{"/Users/angelnicolasmagro/Documents/GitHub/tl2-tp1-2023-NicoMagro/MyApp/cadeteria.csv"}' no fue encontrado.");
+            return new List<Cadeteria>();
         }
     }
 
 
 
-    public override void leerCadete()
+    public override List<Cadete> leerCadete()
     {
         try
         {
@@ -103,24 +113,32 @@ public class AccesoCSV : AccesoADatos
                             Console.WriteLine("La línea no tiene el formato correcto.");
                         }
                     }
+                    System.Console.WriteLine("Cadetes cargados correctamente.");
+                    return listaCadetes;
+                }
+                else
+                {
+                    System.Console.WriteLine("Los cadetes no se cargaron correctamente.");
+                    return new List<Cadete>();
                 }
             }
         }
         catch (FileNotFoundException)
         {
             Console.WriteLine($"El archivo CSV en la ruta '{"/Users/angelnicolasmagro/Documents/GitHub/tl2-tp1-2023-NicoMagro/MyApp/cadetes.csv"}' no fue encontrado.");
+            return new List<Cadete>();
         }
     }
 }
 
 public class AccesoJSON : AccesoADatos
 {
-    public AccesoJSON(string rutaArchivo) : base()
+    public AccesoJSON() : base()
     {
 
     }
 
-    public override void leerCadeteria()
+    public override List<Cadeteria> leerCadeteria()
     {
         try
         {
@@ -130,27 +148,32 @@ public class AccesoJSON : AccesoADatos
 
             Console.WriteLine("Datos desde JSON:");
             Console.WriteLine(json);
+
+            System.Console.WriteLine("Cadeteria cargada correctamente.");
+            return cadeteria;
         }
         catch (FileNotFoundException)
         {
             System.Console.WriteLine($"El archivo CSV en la ruta '{"/Users/angelnicolasmagro/Documents/GitHub/tl2-tp1-2023-NicoMagro/MyApp/cadeteria.json"}' no fue encontrado.");
+            return new List<Cadeteria>();
         }
     }
 
-    public override void leerCadete()
+    public override List<Cadete> leerCadete()
     {
         try
         {
             var json = File.ReadAllText("/Users/angelnicolasmagro/Documents/GitHub/tl2-tp1-2023-NicoMagro/MyApp/cadetes.json");
             //Deserealizo el archivo en una lista de cadetes
             List<Cadete> cadetes = JsonSerializer.Deserialize<List<Cadete>>(json);
-
             Console.WriteLine("Datos desde JSON:");
             Console.WriteLine(json);
+            return cadetes;
         }
         catch (FileNotFoundException)
         {
             System.Console.WriteLine($"El archivo CSV en la ruta '{"/Users/angelnicolasmagro/Documents/GitHub/tl2-tp1-2023-NicoMagro/MyApp/cadetes.json"}' no fue encontrado.");
+            return new List<Cadete>();
         }
     }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -3,49 +3,40 @@ using System.ComponentModel;
 
 Console.WriteLine("Hello, World!, Nico");
 
-string ubicacionCadeteria = "/Users/angelnicolasmagro/Documents/GitHub/tl2-tp1-2023-NicoMagro/MyApp/cadeteria.csv";
-System.IO.StreamReader archivo = new System.IO.StreamReader(ubicacionCadeteria);
-string separador = ",";
-string linea;
-archivo.ReadLine();
-linea = archivo.ReadLine();
-string[] fila = linea.Split(separador);
-string nombreCadeteria = fila[0];
-string telefonoCadeteria = fila[1];
-System.Console.WriteLine("Nombre {0}, Telefono {1}", nombreCadeteria, telefonoCadeteria);
-Cadeteria cadeteria1 = new Cadeteria("Cdetes buenos", "4750843");
+var accesoCSV = new AccesoCSV();
+var accesoJSON = new AccesoJSON();
 
-string ubicacionCadete = "/Users/angelnicolasmagro/Documents/GitHub/tl2-tp1-2023-NicoMagro/MyApp/cadetes.csv";
-System.IO.StreamReader archivo1 = new System.IO.StreamReader(ubicacionCadete);
-archivo1.ReadLine();
+var listaCadeteria = new List<Cadeteria>();
+var listaCadetes = new List<Cadete>();
 
-Cadete c1 = new Cadete();
-Cadete c2 = new Cadete();
+PreguntaCargaArchivos();
 
-int i = 1;
-
-while ((linea = archivo1.ReadLine()) != null)
+void PreguntaCargaArchivos()
 {
-    fila = linea.Split(separador);
-    var id = Convert.ToInt32(fila[0]);
-    string nombre = fila[1];
-    string direccion = fila[2];
-    string telefono = fila[3];
-    System.Console.WriteLine("{0} {1} {2} {3}", id, nombre, telefono, direccion);
-    c1 = new Cadete(id, nombre, direccion, telefono);
-    i++;
+    Console.WriteLine("Como desea realizar la carga de los archivos?");
+    Console.WriteLine("1) archivo CSV");
+    Console.WriteLine("2) archivo JSON");
+    string respuesta = Console.ReadLine();
+    switch (respuesta)
+    {
+        case "1":
+            listaCadeteria = accesoCSV.leerCadeteria();
+            listaCadetes = accesoCSV.leerCadete();
+            break;
+        case "2":
+            listaCadeteria = accesoJSON.leerCadeteria();
+            listaCadetes = accesoJSON.leerCadete();
+            break;
+
+        default:
+            Console.WriteLine("Ingresa un numero valido.");
+            break;
+    }
+
+    InicioInterfaz();
 }
 
-
-// Pedido p1 = new Pedido(1, "Caja Rota", "Nicolas Magro", "La Arboleda", "3815791342", "Casita", "En viaje");
-
-// Pedido p2 = new Pedido(2, "Pesado", "Pedro Pepito", "Recoleta", "3815791343", "Depto 4A", "Entregado");
-
-
-AccesoADatos accesoCadeteria = new AccesoCSV("/Users/angelnicolasmagro/Documents/GitHub/tl2-tp1-2023-NicoMagro/MyApp/cadeteria.csv");
-
-accesoCadeteria.leerCadeteria();
-
-AccesoADatos accesoCadete = new AccesoCSV("/Users/angelnicolasmagro/Documents/GitHub/tl2-tp1-2023-NicoMagro/MyApp/cadetes.csv");
-
-accesoCadete.leerCadete();
+void InicioInterfaz()
+{
+    Console.WriteLine("HOLA :)");
+}
